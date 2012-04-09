40f86802a8718f4ca18426b77118fa076c34453e
Kaarle Rajamaa <kaarle.rajamaa@aalto.fi>
2012-04-09 12:39:13 +0300

    Added single pic view

---
+++ main/static/js/highslide/highslide.config.js
@@ -0,0 +1,33 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'static/js/highslide/graphics/';
+hs.showCredits = false;
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.75;
+hs.align = 'center';
+hs.headingOverlay.position = 'leftpanel';
+
+
+// Add the slideshow controller
+hs.addSlideshow({
+	slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: true,
+	fixedControls: 'fit',
+	overlayOptions: {
+		className: 'large-dark',
+		opacity: 0.6,
+		position: 'bottom center',
+		offsetX: 0,
+		offsetY: -15,
+		hideOnMouseOut: true
+	}
+});
+
+// gallery config object
+var config1 = {
+	slideshowGroup: 'group1',
+	transitions: ['expand', 'crossfade']
+};
